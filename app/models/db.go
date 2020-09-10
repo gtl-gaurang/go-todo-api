27cdf401d7fdd7e4f14fcc0ce4f07c04507f33ac
Dhawal Gandhi <dhawal.gandhi@thegatewaycorp.com>
2020-09-10 20:16:25 +0530

    undo App

--- app/models/db.go
+++ app/models/db.go
@@ -8,18 +8,21 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql" //mysql database driver
 )
 
+// DataSource ..
 type DataSource struct {
 	DB *gorm.DB
 }
 
+var DB *DataSource
+
 // Initialize ... Used for initialize the DB connection
-func (db *DataSource) Initialize(Dbdriver, DbUser, DbPassword, DbPort, DbHost, DbName string) {
+func Initialize(Dbdriver, DbUser, DbPassword, DbPort, DbHost, DbName string) *DataSource {
 	var err error
 	fmt.Println("Database")
 	if Dbdriver == "mysql" {
 		DBURL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", DbUser,
 			DbPassword, DbHost, DbPort, DbName)
-		db.DB, err = gorm.Open(Dbdriver, DBURL)
+		DB.DB, err = gorm.Open(Dbdriver, DBURL)
 		if err != nil {
 			fmt.Printf("Cannot connect to %s database", Dbdriver)
 			log.Fatal("This is the error:", err)
@@ -28,7 +31,7 @@ func (db *DataSource) Initialize(Dbdriver, DbUser, DbPassword, DbPort, DbHost, D
 		}
 	} else if Dbdriver == "postgres" {
 		DBURL := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", DbHost, DbPort, DbUser, DbName, DbPassword)
-		db.DB, err = gorm.Open(Dbdriver, DBURL)
+		DB.DB, err = gorm.Open(Dbdriver, DBURL)
 		if err != nil {
 			fmt.Printf("Cannot connect to %s database", Dbdriver)
 			log.Fatal("This is the error connecting to postgres:", err)
@@ -39,11 +42,13 @@ func (db *DataSource) Initialize(Dbdriver, DbUser, DbPassword, DbPort, DbHost, D
 		fmt.Println("Unknown Driver")
 	}
 
-	db.DB.Debug().AutoMigrate(
+	DB.DB.Debug().AutoMigrate(
 		&User{},
 		//&UserAddress{},
 		//&Task{},
 	)
+
+	return DB
 
 	// Add Foreign Key
 	//db.DB.Model(&UserAddress{}).AddForeignKey("user_id", "users(id)", "CASCADE", "RESTRICT")
